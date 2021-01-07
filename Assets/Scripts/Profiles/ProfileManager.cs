512f7d5692b73472661fc71766b43036e6ca93bf
Rafal Chabasinski <rafal.chabasinski@appunite.com>
2021-01-07 23:16:40 +0100

    profiles
    Signed-off-by: Rafal Chabasinski <rafal.chabasinski@appunite.com>

---
+++ Assets/Scripts/Profiles/ProfileManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Profiles
+{
+    public class ProfileManager
+    {
+        public static Profile[] Profiles = new Profile[3];
+        const string filename = "profiles.xml";
+
+        public static void SerializeProfiles()
+        {
+            var profile = new Profile
+            {
+                Name = "profilo uno",
+                UnlockedLevels = new List<int> {2, 3, 4}
+            };
+            Profiles[1] = profile;
+            var serializer = new XmlSerializer(Profiles.GetType());
+            var textWriter = new XmlTextWriter(filename, Encoding.Default);
+            serializer.Serialize(textWriter, Profiles);
+        }
+        
+        public static void DeserializeProfiles()
+        {
+            var serializer = new XmlSerializer(Profiles.GetType());
+            var textReader = new XmlTextReader(filename);
+            Profiles = (Profile[])serializer.Deserialize(textReader);
+        }
+    }
+}
