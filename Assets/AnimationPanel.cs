d1033ea137395d63e0c2f61a94bf0693d1725db8
Rafal Chabasinski <rafal.chabasinski@appunite.com>
2021-01-08 19:02:27 +0100

    test
    Signed-off-by: Rafal Chabasinski <rafal.chabasinski@appunite.com>

---
+++ Assets/AnimationPanel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnimationPanel : MonoBehaviour
+{
+    public Animator Animator;
+    public static int LoadedSceneIndex = 2;
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Animator.SetTrigger("Cover");
+        }
+        
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            Animator.SetTrigger("Uncover");
+        }
+    }
+
+    public void LoadNewSceneTest()
+    {
+        SceneManager.LoadScene(LoadedSceneIndex);
+        Animator.SetTrigger("Uncover");
+    }
+}
