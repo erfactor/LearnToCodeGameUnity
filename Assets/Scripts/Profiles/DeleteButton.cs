689de3dfe54f62371cbd2b490ff47ffe9b4a6494
Cesarski Maciej <kudlatyfilozof@gmail.com>
2021-01-13 21:45:48 +0100

    delete profile button rework

--- Assets/Scripts/Profiles/DeleteButton.cs
+++ Assets/Scripts/Profiles/DeleteButton.cs
@@ -1,14 +1,34 @@
 ﻿using Profiles;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class DeleteButton : MonoBehaviour
+public class DeleteButton : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
     public int index;
+    ProfileManager profileManager;
+    Animator animator;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        profileManager.DeleteProfile(index);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        print("najechane");
+        animator.SetBool("MouseOn", true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        print("wyjechane");
+        animator.SetBool("MouseOn", false);
+    }
+
     void Start()
     {
-        var button = GetComponent<Button>();
-        var profileManager = GameObject.Find("ProfileManager").GetComponent<ProfileManager>();
-        button.onClick.AddListener(() => profileManager.DeleteProfile(index));
+        profileManager = GameObject.Find("ProfileManager").GetComponent<ProfileManager>();
+        animator = GetComponent<Animator>();
     }
 }
