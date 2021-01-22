e84fe44b3ea47cc84d0fa6cbbbdb726a5d14625c
Cesarski Maciej <kudlatyfilozof@gmail.com>
2021-01-22 02:02:38 +0100

    show/hide board in tutorial

---
+++ Assets/GameObjects/Tutorial/ToggleBoard.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tutorial
+{
+    public class ToggleBoard : MonoBehaviour, INarrative
+    {
+        public GameObject VisiblePosition;
+        public GameObject HiddenPosition;
+
+        public GameObject nextGameObject;
+
+        public static bool IsHidden = true;
+
+        public INarrative Next
+        {
+            get
+            {
+                return nextGameObject.GetComponent<INarrative>();
+            }
+        }
+
+        public void OnFinish()
+        {
+            Next.OnStart();
+        }
+
+        public void OnStart()
+        {
+            StartCoroutine(OnWaiting());
+        }
+
+        public IEnumerator OnWaiting()
+        {
+            var targetLocation = IsHidden ? VisiblePosition.transform.position : HiddenPosition.transform.position;
+            IsHidden = !IsHidden;
+            var movedObject = GameObject.Find("TileLevel");
+
+            for(int i=0; i<40; i++)
+            {
+                var newPosition = Vector3.Lerp(movedObject.transform.position, targetLocation, 0.12f);
+                movedObject.transform.position = newPosition;
+                yield return new WaitForFixedUpdate();
+            }
+            OnFinish();
+            yield break;
+        }
+
+        // Start is called before the first frame update
+
+
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
